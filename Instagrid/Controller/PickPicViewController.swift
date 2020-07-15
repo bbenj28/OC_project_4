c936da7289b0a682dbd8e894788db5dbcccb4c26
Benjamin Breton <bbenjaminbreton@gmail.com>
2020-07-15 08:24:02 +0200

    PickPic controller's reorganisation

--- Instagrid/Controller/PickPicViewController.swift
+++ Instagrid/Controller/PickPicViewController.swift
@@ -7,43 +7,36 @@
 //
 
 import UIKit
-
+// MARK: Init
 class PickPicViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     
+        // MARK: Properties
+    var imagePicker = UIImagePickerController()
+    let grid = Grid()
+    
+        // MARK: Outlets
     @IBOutlet weak var gridView: GridView!
     @IBOutlet var picSquares: [PicSquaresViews]!
     @IBOutlet var layoutsButtons: [UIImageView]!
     @IBOutlet var layoutsSelectedViews: [UIImageView]!
     
-    var imagePicker = UIImagePickerController()
-    let picDisposition = PicDisposition()
-    
+        // MARK: Viewdidload
     override func viewDidLoad() {
         super.viewDidLoad()
-        changeDisposition(0)
+        updateLayoutAndSquares(0)
         // Do any additional setup after loading the view.
     }
-    
-    private func changeDisposition(_ index: Int?) {
-        picDisposition.changeDisposition(index)
-        for i in 0...2 {
-            if picDisposition.selectedLayout == i {
-                layoutsSelectedViews[i].isHidden = false
-            } else {
-                layoutsSelectedViews[i].isHidden = true
-            }
-        }
-        for i in 0...3 {
-            picSquares[i].setView(picDisposition.picSquares[i])
-        }
-        
-    }
+}
 
+// MARK: Choose layout
+extension PickPicViewController {
+    
+    
+        // MARK: Layouts buttons
     @IBAction func layout1IsSelected(_ sender: Any) {
         layoutIsSelected(0)
     }
-    
     @IBAction func layout2IsSelected(_ sender: Any) {
         layoutIsSelected(1)
     }
@@ -51,120 +44,100 @@ class PickPicViewController: UIViewController, UINavigationControllerDelegate, U
         layoutIsSelected(2)
     }
     private func layoutIsSelected(_ index: Int) {
-        if index != picDisposition.selectedLayout {
-            changeDisposition(index)
+        if index != grid.selectedLayout {
+            updateLayoutAndSquares(index)
         }
     }
     
+        // MARK: Change disposition
+    private func updateLayoutAndSquares(_ index: Int?) {
+        grid.changeDisposition(index)
+        for i in 0...2 {
+            if grid.selectedLayout == i {
+                layoutsSelectedViews[i].isHidden = false
+            } else {
+                layoutsSelectedViews[i].isHidden = true
+            }
+        }
+        for i in 0...3 {
+            picSquares[i].setView(grid.picSquares[i])
+        }
+        
+    }
+}
+
+// MARK: Choose pictures
+extension PickPicViewController {
     
     
-    
+        // MARK: Squares buttons
     @IBAction func askForChangePictureInPicSquare0(_ sender: Any) {
         askForChangePictureInPicSquares(0)
     }
-    
     @IBAction func askForChangePictureInPicSquare1(_ sender: Any) {
         askForChangePictureInPicSquares(1)
     }
-    
     @IBAction func askForChangePictureInPicSquare2(_ sender: Any) {
         askForChangePictureInPicSquares(2)
     }
-    
     @IBAction func askForChangePictureInPicSquare3(_ sender: Any) {
         askForChangePictureInPicSquares(3)
     }
-    
     private func askForChangePictureInPicSquares(_ index: Int) {
-        picDisposition.selectedSquare = index
+        grid.selectedSquare = index
         if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
-            print(imagePicker.mediaTypes)
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.allowsEditing = false
-            
-
-            present(imagePicker, animated: true, completion: nil)
+            imagePickerInit()
+        } else {
+            displayingAlert(title: "Photos album not available", text: "The photos album is not available. The application can not add pictures.")
         }
         
     }
     
+        // MARK: Image picker
+    func imagePickerInit() {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         self.dismiss(animated: true, completion: {
             let image = info[UIImagePickerController.InfoKey.originalImage]
-            let picSquare = self.picDisposition.picSquares[self.picDisposition.selectedSquare!]
+            let picSquare = self.grid.picSquares[self.grid.selectedSquare!]
             picSquare.pictureIsSelected = true
-            self.picSquares[self.picDisposition.selectedSquare!].changePicture(image as! UIImage)
+            self.picSquares[self.grid.selectedSquare!].changePicture(image as! UIImage)
         })
-
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
-
-
-// MARK: Generate and share
+// MARK: Swipe to share
 extension PickPicViewController {
+    
+    
+        // MARK: Swipes
     @IBAction func swipeUpGestureRecognized(_ sender: UISwipeGestureRecognizer) {
         if sender.state == .ended {
             if UIDevice.current.orientation.isPortrait {
-                if picDisposition.gridIsReadyToShare {
-                    moveGridAnimation()
-                    generateAndSharePicture()
-                } else {
-                    displayingAlert(title: "Choose pictures", text: "All squares have to be full in the choosen layout. Please, choose pictures.")
-                }
-                
+                swipeActions()
             }
         }
     }
-    private func returnDeletedGridAnimation() {
-        picDisposition.delete()
-        changeDisposition(nil)
-        returnGridAnimation()
-    }
-    private func returnGridAnimation() {
-        gridAnimation(.identity)
-    }
-    private func gridAnimation(_ transform: CGAffineTransform) {
-        UIView.animate(withDuration: 0.3, animations: {
-            self.gridView.transform = transform
-        })
+    private func swipeActions() {
+        if grid.gridIsReadyToShare {
+            moveGridAnimation()
+            generateAndSharePicture()
+        } else {
+            displayingAlert(title: "Choose pictures", text: "All squares have to be full in the choosen layout. Please, choose pictures.")
+        }
     }
     
-    private func generateAndSharePicture() {
-        let image = generatePicture()
-        let ac = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        ac.completionWithItemsHandler = { (activity, success, items, error) in
-            if success {
-                self.returnDeletedGridAnimation()
-            } else {
-                self.returnGridAnimation()
-            }
-             //print(success ? "SUCCESS!" : "FAILURE")
-        }
-
-        present(ac, animated: true)
-    }
-    private func generatePicture() -> UIImage {
-        return gridView.asImage()
-    }
+        // MARK: Grid Animations
     private func moveGridAnimation() {
-        gridAnimation(moveGrid())
+        gridAnimation(gridDisparition())
     }
-    private func moveGrid() -> CGAffineTransform {
+    private func gridDisparition() -> CGAffineTransform {
         var translationX: CGFloat
         var translationY: CGFloat
         if UIDevice.current.orientation.isPortrait {
@@ -178,6 +151,39 @@ extension PickPicViewController {
         }
         let translation = CGAffineTransform(translationX: translationX, y: translationY)
         return translation
+    }
+    private func returnDeletedGridAnimation() {
+        grid.delete()
+        updateLayoutAndSquares(nil)
+        returnGridAnimation()
+    }
+    private func returnGridAnimation() {
+        gridAnimation(.identity)
+    }
+    private func gridAnimation(_ transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.gridView.transform = transform
+        })
+    }
+    
+        // MARK: Generate and share
+    private func generateAndSharePicture() {
+        let image = generatePicture()
+        sharePicture(image)
+    }
+    private func generatePicture() -> UIImage {
+        return gridView.asImage()
+    }
+    private func sharePicture(_ image: UIImage) {
+        let ac = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        ac.completionWithItemsHandler = { (activity, success, items, error) in
+            if success {
+                self.returnDeletedGridAnimation()
+            } else {
+                self.returnGridAnimation()
+            }
+        }
+        present(ac, animated: true)
     }
 }
 
