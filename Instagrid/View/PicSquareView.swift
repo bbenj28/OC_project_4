c0fc67127882f9907fb7f2e63cec37218f56777f
Benjamin Breton <bbenjaminbreton@gmail.com>
2020-08-01 09:14:33 +0200

    clean code (view)

--- Instagrid/View/PicSquareView.swift
+++ Instagrid/View/PicSquareView.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class PicSquareView: UIButton {
+    /// Change picSquare regarding selected layout and picture.
     func setView(_ picSquare: PicSquare) {
         self.isHidden = picSquare.isHidden
         let background = UIImage(imageLiteralResourceName: "\(picSquare.disposition.backgroundName()) Square Background")
@@ -16,33 +17,27 @@ class PicSquareView: UIButton {
         changeRatio(CGFloat(picSquare.disposition.ratio()))
         imageButton(picSquare.imageButton())
     }
+    
+    /// Change picSquare's image regarding selected picture.
     private func imageButton(_ data: Data?) {
-        
         if let verifiedData = data {
             if let verifiedImage = UIImage(data: verifiedData) {
-                
-                //self.contentVerticalAlignment = .fill
-                //self.contentHorizontalAlignment = .fill
- 
                 self.setImage(verifiedImage, for: .normal)
             } else {
-                
                 imagePlus()
             }
         } else {
             imagePlus()
         }
-        
     }
     private func imagePlus() {
-        
         //self.contentVerticalAlignment = .center
         //self.contentHorizontalAlignment = .center
- 
         let image = UIImage(imageLiteralResourceName: "Plus")
         self.setImage(image, for: .normal)
     }
     
+    /// Change picSquare's ratio regarding selected layout.
     private func changeRatio(_ multiplier: CGFloat) {
         for constraint in constraints {
             if constraint.identifier == "PicSquareRatio" {
@@ -55,5 +50,4 @@ class PicSquareView: UIButton {
             }
         }
     }
-
 }
